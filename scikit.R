c13fba1cc2e772ebbd861ae4392fd78cb23fe3be
WijnandNuij <wijnandnuij@gmail.com>
2014-07-31 13:53:42 +0200

    started with scikit, trying randomForests / extraTrees / svm / neuralnetworks

--- scikit.R
+++ scikit.R
@@ -1,10 +1,14 @@
 
-loadData <- function(file = '/home/wijnand/R_workspace_scikit/resources/train.csv')
+runML <- function(file = '/home/wijnand/R_workspace_scikit/resources/train.csv')
 {
         set.seed(1234)
         
         # read training data
         data <- read.csv(file, header = F)
+        
+        #data <- predict(preProcess(data, method="scale"), newdata = data)
+        #data <- predict(preProcess(data, method="pca", thresh = 0.95), newdata = data)
+        
         outcome <- read.csv('/home/wijnand/R_workspace_scikit/resources/trainLabels.csv', header = F)
         colnames(outcome) <- 'outcome'
         outcome$outcome <- factor(outcome$outcome)
@@ -15,11 +19,25 @@ loadData <- function(file = '/home/wijnand/R_workspace_scikit/resources/train.cs
         data <- data[order(runif(nrow(data))),]
         
         library(caret)
-        inTrain <- createDataPartition(data$outcome, p = 0.7, list=F)
+        inTrain <- createDataPartition(data$outcome, p = 0.9, list=F)
         training <- data[inTrain,]
         testing <- data[-inTrain,]
-        
 
+        ### GBM ###
+        print("generalized boosted model")
+        trainedModel <- train(outcome ~ . , data=training,
+                            method="gbm",
+                            verbose = F)
+        prediction <- predict(trainedModel, testing)
+        print(confusionMatrix(prediction, testing$outcome)$overall[1])
+        
+        ### KNN NEAREST NEIGHBORS ###
+        print("k-nearest neighbors")
+        prediction <- knn(train = training[,!(names(training) %in% 'outcome')],
+                          test = testing[,!(names(testing) %in% 'outcome')], 
+                          cl = training$outcome, 
+                          k = 5)
+        print(confusionMatrix(prediction, testing$outcome)$overall[1])
         
         ### RANDOM FOREST ###
         print("random forest")
@@ -35,10 +53,11 @@ loadData <- function(file = '/home/wijnand/R_workspace_scikit/resources/train.cs
         ### EXTREME RANDOMIZED TREES ###
         library(extraTrees)
         options( java.parameters = "-Xmx2g" )
-        print("extreme randomized trees")
+        print("extreme randomized tree")
         trainedModel <- extraTrees(training[,!(names(training) %in% 'outcome')], 
                             training$outcome, 
-                            ntree = 1500, 
+                            mtry = 10,
+                            ntree = 1000, 
                             numRandomCuts = 5, 
                             numThreads = 4)
         prediction <- predict(trainedModel, testing[,!(names(testing) %in% 'outcome')])
