c9f3ab6a0c2a3588c37d864867d9f77cc426080a
WijnandNuij <wijnandnuij@gmail.com>
2014-09-02 16:31:24 +0200

    caret for-loop first results

--- runAllModels.R
+++ runAllModels.R
@@ -32,50 +32,66 @@ runAllModels <- function(baseDir = '/home/wijnand/R_workspace_scikit')
         
         
         ### CARET MODEL LOOP ###
-        library(caret)
+        library(caret);
+        library(doSNOW);
+        registerDoSNOW(makeCluster(4, outfile=""))
         
-        name <- c("ada", "bagFDA", "C5.0", "C5.0Cost", "C5.0Rules", "C5.0Tree", "CSimca", "FH.GBML", 
-                  "FRBCS.CHI", "FRBCS.W", "GFS.GCCL", "gpls", "hda", "hdda", "J48", "JRip", "lda", "lda2", 
-                  "Linda", "LMT", "LogitBoost", "lssvmLinear", "lssvmPoly", "lssvmRadial", "lvq", "mda", 
+        name <- c("hdda", "JRip", "lda", "lda2", "J48", "LMT", "LogitBoost", "lssvmRadial", "lvq",  
+                  "rda", "rFerns", "rocc", "rpartCost", "rrlda", "RSimca", "sda",  
+                  "slda", "sparseLDA", "stepLDA", "stepQDA", "svmRadialWeights", "vbmpRadial",
+                  "ada", "bagFDA", "C5.0", "C5.0Cost", "C5.0Rules", "C5.0Tree", "CSimca", "gpls", "hda", 
                   "Mlda", "multinom", "nb", "oblique.tree", "OneR", "ORFlog", "ORFpls", "ORFridge", "ORFsvm", 
                   "pam", "PART", "pda", "pda2", "PenalizedLDA", "plr", "protoclass", "qda", "QdaCov", "rbf", 
-                  "rda", "rFerns", "RFlda", "rocc", "rpartCost", "rrlda", "RSimca", "sda", "sddaLDA", "sddaQDA", 
-                  "SLAVE", "slda", "smda", "sparseLDA", "stepLDA", "stepQDA", "svmRadialWeights", "vbmpRadial")
+                  "FH.GBML", "FRBCS.CHI", "FRBCS.W", "GFS.GCCL", 
+                  
+                  "Linda", "lssvmLinear", "lssvmPoly", "mda", "sddaLDA", "sddaQDA",  "SLAVE", "smda")
+        
+        # fda really slow
+        # RDlda not available
+        # Linda, lssvmLinear, lssvmPoly, mda
         
         #name <- c("C5.0", "C5.0Cost")
         
         ctrl = trainControl(method = "repeatedcv", 
-                                 number = 10, 
-                                 repeats = 2)
+                            number = 10, 
+                            repeats = 2)
         
         
         for(s in name)
         {
-                startTime <- Sys.time()
-                trainedModel <- train(outcome ~ . , data=training,
-                                      method = s,
-                                      tuneLength = 12, 
-                                      trControl = ctrl,
-                                      metric = "Kappa")
-                predictionSomeModel <- predict(trainedModel, testing)
-                
-                print(paste0(s, " : ", confusionMatrix(predictionSomeModel, testing$outcome)$overall[1]))
-                
-                ## WRITE TO MODEL FILE
-                sink(file = paste0(baseDir, '/models/', s, '.txt'))
-                print(Sys.time() - startTime)
-                print('===============')
-                print(trainedModel)
-                print('===============')
-                print(confusionMatrix(predictionSomeModel, testing$outcome))
-                sink()
-                
-                ## WRITE TO OVERALL RESULTS FILE
-                sink(file = paste0(baseDir, '/models/1_overall_results.txt'), append = T)
-                print(paste0(s, ';',
-                             confusionMatrix(predictionSomeModel, testing$outcome)$overall[1], ';',
-                             confusionMatrix(predictionSomeModel, testing$outcome)$overall[2]))
-                sink()
-                ##
+                resultFile <- paste0(baseDir, '/models/', s, '.txt')
+                if(file.exists(resultFile))
+                {
+                        print(paste0('skipping model: ', s, ' , already done.'))
+                }
+                else
+                {
+                        startTime <- Sys.time()
+                        trainedModel <- train(outcome ~ . , data=training,
+                                              method = s,
+                                              tuneLength = 12, 
+                                              trControl = ctrl,
+                                              metric = "Kappa")
+                        predictionSomeModel <- predict(trainedModel, testing)
+                        
+                        print(paste0(s, " : ", confusionMatrix(predictionSomeModel, testing$outcome)$overall[1]))
+                        
+                        ## WRITE TO MODEL FILE
+                        sink(file = resultFile)
+                        print(Sys.time() - startTime)
+                        print('===============')
+                        print(trainedModel)
+                        print('===============')
+                        print(confusionMatrix(predictionSomeModel, testing$outcome))
+                        sink()
+                        
+                        ## WRITE TO OVERALL RESULTS FILE
+                        sink(file = paste0(baseDir, '/models/1_overall_results.txt'), append = T)
+                        print(paste0(s, ';',
+                                     confusionMatrix(predictionSomeModel, testing$outcome)$overall[1], ';',
+                                     confusionMatrix(predictionSomeModel, testing$outcome)$overall[2]))
+                        sink()
+                        ##
+                }
         }
 }
